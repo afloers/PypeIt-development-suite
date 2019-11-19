57597a46521fd18b99a825a65f7ae1e7ceac0890
feigewang <fgwang.astro@gmail.com>
2019-11-18 21:33:11 -0700

    Merge branch 'xshooter_redux' of https://github.com/pypeit/PypeIt-development-suite into xshooter_redux
    # Conflicts:
    #	dev_algorithms/sensfunc/dev_flux_tell.py

--- dev_algorithms/sensfunc/fire_flux.py
+++ dev_algorithms/sensfunc/fire_flux.py
@@ -16,23 +16,29 @@ fileroot = 'J0100_hz6_FIRE'
 #          star_type='A0', star_mag=9.92,
 #          do_sens=False, do_flux=True, do_stack=True, do_tell=True, disp=False, debug=False)
 
-## J0313-1806
-qsoname = 'J0313-1806'
-sci_path = '/d2/Feige/Dropbox/OBS_DATA/FIRE/Echelle/ut191111/Science'
-std_path = '/d2/Feige/Dropbox/OBS_DATA/FIRE/Echelle/ut191111/Science'
-stdfile = 'spec1d_fire_0157-GD71_FIRE_8590Nov02T203203.000.fits'
-z_qso = 7.65
+std_path = '/d2/Feige/Dropbox/OBS_DATA/FIRE/Echelle/SENSFUNC'
+#stdfile = 'spec1d_fire_0157-GD71_FIRE_8590Nov02T203203.000.fits'
+stdfile = 'spec1d_fire_0162-GD71_FIRE_8590Nov02T204709.000.fits'
 tell_method = 'qso'
+
+## J0313-1806
+sci_path = '/d2/Feige/Dropbox/OBS_DATA/FIRE/Echelle/J0313m1806'
+z_qso = 7.6
 fileroot = 'J0313-1806_FIRE'
 flux_tell(sci_path, stdfile, std_path=std_path,instrument=instrument, fileroot=fileroot, z_qso=z_qso, tell_method=tell_method,
-          do_sens=True, do_flux=True, do_stack=True, do_tell=True, disp=False, debug=False)
+          do_sens=False, do_flux=True, do_stack=True, do_tell=False, disp=False, debug=False)
 
 ## J0038-0653
-qsoname = 'J0038-0653'
+sci_path = '/d2/Feige/Dropbox/OBS_DATA/FIRE/Echelle/J0038m0653'
 z_qso = 7.1
-tell_method = 'qso'
 fileroot = 'J0038-0653_FIRE'
-#flux_tell(sci_path, stdfile, instrument=instrument, fileroot=fileroot, z_qso=z_qso, tell_method=tell_method,
-#          star_type='A0', star_mag=9.48,
-#          do_sens=False, do_flux=True, do_stack=True, do_tell=True, disp=False, debug=False)
+flux_tell(sci_path, stdfile, std_path=std_path,instrument=instrument, fileroot=fileroot, z_qso=z_qso, tell_method=tell_method,
+          do_sens=False, do_flux=True, do_stack=True, do_tell=False, disp=False, debug=False)
+
+## J1007p2125
+sci_path = '/d2/Feige/Dropbox/OBS_DATA/FIRE/Echelle/J1007p2125'
+z_qso = 7.5
+fileroot = 'J1007+2115_FIRE'
+flux_tell(sci_path, stdfile, std_path=std_path,instrument=instrument, fileroot=fileroot, z_qso=z_qso, tell_method=tell_method,
+          do_sens=False, do_flux=True, do_stack=True, do_tell=False, disp=False, debug=False)
 
