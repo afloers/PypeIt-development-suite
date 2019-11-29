ad6811a0d25c129c66cf35525300523c155a88cf
Feige Wang <fgwang.astro@gmail.com>
2019-11-29 10:46:43 -0800

    new grid

--- dev_algorithms/sensfunc/flux_coadd_tell.py
+++ dev_algorithms/sensfunc/flux_coadd_tell.py
@@ -25,7 +25,7 @@ def get_sens_from_file(std1dfile=None, instrument='GNIRS', star_type=None, star_
     pca_file = os.path.join(basedir, 'Dropbox/PypeIt_Redux/qso_pca_1200_3100.pckl')
 
     if (instrument=='GNIRS') or (instrument=='NIRES'):
-        telgridfile = os.path.join(basedir, 'Dropbox/PypeIt_Redux/TelFit_MaunaKea_3100_26100_R20000.fits')
+        telgridfile = os.path.join(basedir, 'Dropbox/PypeIt_Redux/TelFit_MaunaKea_3100_26100_R20000_fixed_big.fits')
     elif (instrument == 'XSHOOTER_VIS') or (instrument == 'GMOS-S'):
         telgridfile = os.path.join(basedir,
                                    'Dropbox/PypeIt_Redux/XSHOOTER/TelFit_Paranal_VIS_4900_11100_R25000.fits')
@@ -33,7 +33,7 @@ def get_sens_from_file(std1dfile=None, instrument='GNIRS', star_type=None, star_
         telgridfile = os.path.join(basedir,
                                    'Dropbox/PypeIt_Redux/XSHOOTER/TelFit_Paranal_NIR_9800_25000_R25000.fits')
     else:
-        telgridfile = os.path.join(basedir, 'Dropbox/PypeIt_Redux/TelFit_MaunaKea_3100_26100_R20000.fits')
+        telgridfile = os.path.join(basedir, 'Dropbox/PypeIt_Redux/TelFit_MaunaKea_3100_26100_R20000_fixed_big.fits')
         msgs.warn('No telluric grid is found. Using MaunaKea!')
     msgs.info('Using {:}'.format(telgridfile))
 
@@ -120,7 +120,7 @@ def flux_tell(sci_path, stdfile, spec1dfiles=None, std_path=None, fileroot=None,
         msgs.info('Loading sensfile {:}'.format(sensfile))
 
         if (instrument=='GNIRS') or (instrument=='NIRES'):
-            telgridfile = os.path.join(basedir, 'Dropbox/PypeIt_Redux/TelFit_MaunaKea_3100_26100_R20000.fits')
+            telgridfile = os.path.join(basedir, 'Dropbox/PypeIt_Redux/TelFit_MaunaKea_3100_26100_R20000_fixed_big.fits')
         elif (instrument == 'XSHOOTER_VIS') or (instrument == 'GMOS-S'):
             telgridfile = os.path.join(basedir,
                                        'Dropbox/PypeIt_Redux/XSHOOTER/TelFit_Paranal_VIS_4900_11100_R25000.fits')
@@ -128,7 +128,7 @@ def flux_tell(sci_path, stdfile, spec1dfiles=None, std_path=None, fileroot=None,
             telgridfile = os.path.join(basedir,
                                        'Dropbox/PypeIt_Redux/XSHOOTER/TelFit_Paranal_NIR_9800_25000_R25000.fits')
         else:
-            telgridfile = os.path.join(basedir, 'Dropbox/PypeIt_Redux/TelFit_MaunaKea_3100_26100_R20000.fits')
+            telgridfile = os.path.join(basedir, 'Dropbox/PypeIt_Redux/TelFit_MaunaKea_3100_26100_R20000_fixed_big.fits')
             msgs.warn('No telluric grid is found. Using MaunaKea!')
 
     ### Apply the sensfunc to all spectra (only sensfunc but not tellluric)
