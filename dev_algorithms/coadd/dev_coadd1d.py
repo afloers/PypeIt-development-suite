e6c73115e33bceb738b085d62ca4ddb854e7a56e
feigewang <fgwang.astro@gmail.com>
2020-01-10 14:43:01 -0700

    deleting some Feige's examples

--- dev_algorithms/coadd/dev_coadd1d.py
+++ dev_algorithms/coadd/dev_coadd1d.py
@@ -70,27 +70,6 @@ def read_gmos_stack():
 
     return waves, fluxes, ivars, masks, header
 
-def nires_fnames():
-    datapath = os.path.join(os.getenv('HOME'),'Dropbox/PypeIt_Redux/NIRES/NIRES_May19/Science/')
-    fnames = [datapath+'spec1d_flux_s190519_0037-J1007+2115_NIRES_2019May19T055221.895.fits',
-              datapath+'spec1d_flux_s190519_0038-J1007+2115_NIRES_2019May19T055923.665.fits',
-              datapath+'spec1d_flux_s190519_0041-J1007+2115_NIRES_2019May19T062048.865.fits',
-              datapath+'spec1d_flux_s190519_0042-J1007+2115_NIRES_2019May19T062750.635.fits',
-              datapath+'spec1d_flux_s190519_0045-J1007+2115_NIRES_2019May19T064943.885.fits',
-              datapath+'spec1d_flux_s190519_0046-J1007+2115_NIRES_2019May19T065646.165.fits',
-              datapath+'spec1d_flux_s190519_0049-J1007+2115_NIRES_2019May19T071920.215.fits',
-              datapath+'spec1d_flux_s190519_0050-J1007+2115_NIRES_2019May19T072621.985.fits',
-              datapath+'spec1d_flux_s190519_0053-J1007+2115_NIRES_2019May19T074819.315.fits',
-              datapath+'spec1d_flux_s190519_0054-J1007+2115_NIRES_2019May19T075521.595.fits',
-              datapath+'spec1d_flux_s190519_0057-J1007+2115_NIRES_2019May19T081918.265.fits',
-              datapath+'spec1d_flux_s190519_0058-J1007+2115_NIRES_2019May19T082620.545.fits']
-
-    gdobj = ['OBJ0001','OBJ0001','OBJ0001','OBJ0001',
-             'OBJ0001','OBJ0001','OBJ0001','OBJ0001',
-             'OBJ0001', 'OBJ0001','OBJ0001','OBJ0001']
-
-    return fnames, gdobj
-
 def read_nires_stack(order=None):
 
     fnames, gdobj = nires_fnames()
